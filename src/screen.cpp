a5790f8cfedcc2977a5a7f46f4db43fa632d717c
Chris Gibson <cg@zoox.com>
2017-10-10 18:01:34 -0700

    Added missing NANOGUI_CURSOR_DISABLED check in screen.cpp

--- src/screen.cpp
+++ src/screen.cpp
@@ -345,8 +345,10 @@ void Screen::initialize(GLFWwindow *window, bool shutdownGLFWOnDestruct) {
     mProcessEvents = true;
     __nanogui_screens[mGLFWWindow] = this;
 
+#if !defined(NANOGUI_CURSOR_DISABLED)
     for (int i=0; i < (int) Cursor::CursorCount; ++i)
         mCursors[i] = glfwCreateStandardCursor(GLFW_ARROW_CURSOR + i);
+#endif
 
     /// Fixes retina display-related font rendering issue (#185)
     nvgBeginFrame(mNVGContext, mSize[0], mSize[1], mPixelRatio);
